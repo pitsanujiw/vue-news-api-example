7d9a3520651192a1a54896a53bd4de5defb3c607
pitsanujiw <pitsanujill12@gmail.com>
2021-12-29 16:29:18 +0700

    feat(constant): add constant for news module

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="text-h5">Manatal News</div>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
