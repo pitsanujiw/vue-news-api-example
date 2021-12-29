7c8e246e0e5035f5fac368fd26968ec8e93f309e
pitsanujiw <pitsanujill12@gmail.com>
2021-12-29 16:33:11 +0700

    feat(components): add skeleton loader

---
+++ src/components/SkeletonLoader.vue
@@ -0,0 +1,21 @@
+<template>
+      <v-layout row class="pa-3 justify-start">
+      <v-skeleton-loader
+        v-for="n in 3"
+        :key="n"
+        class="mx-auto"
+        type="card, article"
+        min-width="350"
+      ></v-skeleton-loader>
+    </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'SkeletonLoader',
+};
+</script>
+
+<style>
+
+</style>
