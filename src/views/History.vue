ec7f19277c9cb263adaecb84a1417f0f464e35c3
pitsanujiw <pitsanujill12@gmail.com>
2022-01-01 11:33:17 +0700

    feat(history): add history page

---
+++ src/views/History.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    <div
+      class="d-flex align-center mx-auto justify-center"
+      style="height: calc(100vh - 125px)"
+      v-if="!getHistories.length"
+    >
+      <div>
+        <div class="mr-10 ml-10 d-flex justify-center text-h7 text-center">
+          Opps! Your histories is empty.
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      <div class="text-h6 mb-5 mt-md-2">HISTORY</div>
+      <history-table />
+    </div>
+  </v-container>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+import HistoryTable from '@/components/HistoryTable.vue';
+
+export default {
+  name: 'History',
+  data: () => ({}),
+  components: { HistoryTable },
+  computed: {
+    ...mapGetters(['getHistories']),
+  },
+};
+</script>
+
+<style>
+</style>
