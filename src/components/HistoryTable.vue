9da75d49eb65b048f8a629c3443087e3680851fb
pitsanujiw <pitsanujill12@gmail.com>
2022-01-01 11:32:48 +0700

    feat(history): add component history table

---
+++ src/components/HistoryTable.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-simple-table dark>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <td>Title</td>
+          <td>Source</td>
+          <td>Autdor</td>
+          <td>Visitor dates</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, key) in getHistories" :key="key">
+          <td>{{ item.history.title }}</td>
+          <td>{{ item.history.source.name }}</td>
+          <td>{{ item.history.author || '-' }}</td>
+          <td>{{ onChangeFormatDate(item.date) }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+import { onChangeFormatDate } from '@/utils';
+
+export default {
+  name: 'HistoryTable',
+  data: () => ({}),
+  methods: {
+    onChangeFormatDate,
+  },
+  computed: {
+    ...mapGetters(['getHistories']),
+  },
+};
+</script>
+
+<style>
+</style>
